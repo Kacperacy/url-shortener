8e37e61bca3b472579a59d9c76862e108664d8f0
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-11 14:58:12 +0100

    swagger added for tests

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -18,14 +18,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
-
 
 app.MapPost("/shorten", async (ShortenedUrlRequest request, ApplicationDbContext dbContext, UrlShorteningService urlShorteningService, HttpContext httpContext) =>
 {
